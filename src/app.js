781cbb60e5e7e6f50f5aed1769a75b9fbbec6b5f
Sussanara <sussanaracely@gmail.com>
2022-06-27 23:23:01 +0000

    Random-Card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.genCard = function() {
+    let suit = ["♥", "♠", "♣", "♦"];
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    function randomCard() {
+      let randomsuit = suit[Math.floor(Math.random() * suit.length)];
+      let randomnumbers = numbers[Math.floor(Math.random() * numbers.length)];
+
+      document.querySelector(".icono-top").innerHTML = randomsuit;
+      document.querySelector(".number").innerHTML = randomnumbers;
+      document.querySelector(".icono-bottom").innerHTML = randomsuit;
+    }
+
+    randomCard();
+  };
+
+  window.genCard();
 };
